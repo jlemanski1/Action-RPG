5deb5e0aab557f32245ea079d725992c04157268
Jonathan L <jlemanski1@gmail.com>
2018-07-25 17:49:40 -0600

    Quests
    Made small changes to how the Quest scriptable objects are created

--- RPG/Assets/Scripts/Quests/Quest.cs
+++ RPG/Assets/Scripts/Quests/Quest.cs
@@ -6,9 +6,19 @@ using UnityEngine;
 public class Quest : ScriptableObject
 {
     new public string name = "New Quest";
-    public string questgiver;
+
+    public GameObject questGiver;
+
+    [Header("Quest Conditions")]
+    public Transform destionation;      // Location to reach to end quest
+    public int count;                   // Number of items to collect to end quest
+
+
     [TextArea]
     public string desc = "Description/Directive";
+
+    [TextAreaa]
+    public string story = "Description/Narrative";  // The story written in the journal for the quest
 
     //Make a QuestManager that handles active quests & opened quests
         //Make UI for multiple quests
